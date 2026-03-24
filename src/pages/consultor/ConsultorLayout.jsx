9a5ab26f1ebe4df71bb3a448075616f1cc4ef042
Henrique de Lima Paiva <henrique@nexus.com.br>
2026-03-24 17:41:58 -0300

    fix: corrige logout para redirecionar para tela Splash

--- src/pages/consultor/ConsultorLayout.jsx
+++ src/pages/consultor/ConsultorLayout.jsx
@@ -22,7 +22,7 @@ export default function ConsultorLayout() {
 
   const handleLogout = () => {
     logout();
-    navigate("/");
+    window.location.href = "/";
   };
 
   const handleVoltar = () => {
